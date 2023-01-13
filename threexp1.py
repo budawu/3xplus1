36565ce3ca77043947e369330d691cf7e0ec4899
Expector <expector-hutch@outlook.com>
2023-01-13 18:14:55 +0800

    添加了文言文版实例

--- threexp1.py
+++ threexp1.py
@@ -2,14 +2,10 @@ def threexp1(x: int):
     while x != 1:
         if x % 2 == 0:
             x = x // 2
-            print(x)
         else:
             x = 3 * x + 1
-            print(x)
+        print(x)
     print('OK')
 
-def main():
-    threexp1(27)
-
 if __name__ == "__main__":
-    main()
+    threexp1(27)
