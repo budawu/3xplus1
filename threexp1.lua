78a58e7019ff7f24bc3a655b24e5953766d18d72
Expector <expector-hutch@outlook.com>
2023-01-15 12:13:34 +0800

    Rewriting logic maintained by myself

--- threexp1.lua
+++ threexp1.lua
@@ -2,11 +2,10 @@ function threexp1(x)
     while x ~= 1 do
         if x % 2 == 0 then
             x = x // 2
-            print(x)
         else
             x = 3 * x + 1
-            print(x)
         end
+        print(x)
     end
     print("OK")
 end
--- threexp1.rs
+++ threexp1.rs
@@ -2,11 +2,10 @@ fn threexp1(mut x: isize) {
     while x != 1 {
         if x % 2 == 0 {
             x = x / 2;
-            println!("{0}", x);
         } else {
             x = 3 * x + 1;
-            println!("{0}", x);
         }
+        println!("{0}", x);
     }
     println!("OK");
 }
